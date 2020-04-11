53f60b1635d38c358f433878196f30c2edf627e2
cambronero10 <cambronero.villalta@gmail.com>
2020-04-10 19:44:20 -0700

    Skeleton code for Customer.java

---
+++ SourceCode/Customer.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Customer {
+
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String address;
+    private int cardNumber;
+    private int pinNumber;
+
+    public void goToATM(ATM atm) {
+
+    }
+
+    public void goToBranch(Branch branch) {
+
+    }
+}
