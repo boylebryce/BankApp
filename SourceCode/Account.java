3f1ea8d80ddc46bce2bace54ed3f012ce768db88
Almasilva27 <alma.silva27@yahoo.com>
2020-04-10 20:51:47 -0700

    skeleton class for Account

---
+++ SourceCode/Account.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class Account {
+    private int accountNumber;
+    private String accountType;
+    private boolean flag;
+    private float balance;
+    private List<Card> cards;
+    private String primaryUser;
+    private String authorizedUser;
+    private List<Transaction> transactions;
+    private String reasonForLock;
+
+    public void updateAccount() {
+
+    }
+
+    public void updateBalance() {
+
+    }
+
+    public void updateUser() {
+
+    }
+
+    public void addCheck() {
+
+    }
+
+    public void addToTransactions() {
+
+    }
+}
+
+
