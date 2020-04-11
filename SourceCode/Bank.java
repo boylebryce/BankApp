8512fcf132b9176c376e942445a57af79b61a207
Almasilva27 <alma.silva27@yahoo.com>
2020-04-10 20:59:26 -0700

    skeleton class for Bank

---
+++ SourceCode/Bank.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class Bank {
+    private List<Branch> branches;
+    private List<Customer> customers;
+    private List<Account> accounts;
+    private List<Transaction> transactions;
+
+    private void sendMoneyToATM() {
+
+    }
+
+    private void sendMoneyToBranch() {
+
+    }
+
+    private void newBranch(Branch newBranch) {
+
+    }
+
+    private void validateRequest() {
+
+    }
+
+    public void lockAccount() {
+
+    }
+}
