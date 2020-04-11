0186d66fb6c599207f217f2a24a13bb9dcd7a7d2
cambronero10 <cambronero.villalta@gmail.com>
2020-04-10 20:18:33 -0700

    Skeleton code for Alert.java

---
+++ SourceCode/Alert.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Alert {
+
+    public void reportPriorityLevel() {
+
+    }
+
+    private void alertSecurity() {
+
+    }
+
+    private void alertMaintenance() {
+
+    }
+
+    public void determineAlertType() {
+
+    }
+
+    private void alertFraudDepartment() {
+
+    }
+}
