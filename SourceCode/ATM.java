b188b9b21e5b351aa56f3c417d05fc12dc2f25bc
Almasilva27 <alma.silva27@yahoo.com>
2020-04-10 19:11:48 -0700

    skeleton class for ATM

---
+++ SourceCode/ATM.java
@@ -0,0 +1,23 @@
+public class ATM {
+    private float amountOfMoney;
+    private int amountOfPaper;
+    private int amountOfInk;
+
+    private String sensor;
+    private String status;
+    private String operatingState;
+    private int amountOfMoneyDeposited;
+    private int transactionNumber;
+    private String address;
+    private int usersCardNumber;
+    private String typeOfAlert;
+
+
+    public void sendAlert() {
+        return;
+    }
+
+    public void takePhoto() {
+        return;
+    }
+}
