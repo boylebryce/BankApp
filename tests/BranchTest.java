94649e638ef08b2e95676bb3b409346e0ebc8950
Bryce Boyle <boylebryce@gmail.com>
2020-04-22 23:44:54 -0700

    Unit tests initial commit

---
+++ tests/BranchTest.java
@@ -0,0 +1,32 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BranchTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addAuthorized() {
+    }
+
+    @Test
+    public void unlockAccount() {
+    }
+
+    @Test
+    public void newATM() {
+    }
+
+    @Test
+    public void lockAccount() {
+    }
+}
---
+++ tests/IBankTest.java
@@ -0,0 +1,64 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IBankTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void deposit() {
+    }
+
+    @Test
+    public void withdraw() {
+    }
+
+    @Test
+    public void showBalance() {
+    }
+
+    @Test
+    public void printBalance() {
+    }
+
+    @Test
+    public void createTransaction() {
+    }
+
+    @Test
+    public void authenticateUser() {
+    }
+
+    @Test
+    public void viewReceipt() {
+    }
+
+    @Test
+    public void printReceipt() {
+    }
+
+    @Test
+    public void alertAuthorities() {
+    }
+
+    @Test
+    public void takeCard() {
+    }
+
+    @Test
+    public void validateCheck() {
+    }
+
+    @Test
+    public void transfer() {
+    }
+}
