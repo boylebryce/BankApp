fed0ec1ad15b4dc7349578d07fa7ff393bee2f8e
Bryce Boyle <boylebryce@gmail.com>
2020-05-11 02:33:40 -0700

    Add ATM data string tests

--- tests/ATMTest.java
+++ tests/ATMTest.java
@@ -1,3 +1,6 @@
+import impl.ATM;
+import impl.Bank;
+import impl.BankBranch;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -5,9 +8,21 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ATMTest {
+    Bank testBank;
+    BankBranch testBranch;
+    ATM testATM;
+    long testAccountID;
 
     @Before
     public void setUp() throws Exception {
+        testBank = new Bank("Test Bank");
+        testBranch = new BankBranch("Test Branch");
+        testBank.newBranch(testBranch);
+
+        testATM = new ATM();
+        testBranch.newATM(testATM);
+
+        testAccountID = testBranch.createAccount("Test Name");
     }
 
     @After
@@ -15,10 +30,81 @@ public class ATMTest {
     }
 
     @Test
-    public void sendAlert() {
+    public void getId() {
     }
 
     @Test
-    public void takePhoto() {
+    public void setBankBranch() {
+    }
+
+    @Test
+    public void setATMMaintenancePolicy() {
+    }
+
+    @Test
+    public void setMoneyLevel() {
+    }
+
+    @Test
+    public void setInkLevel() {
+    }
+
+    @Test
+    public void setPaperLevel() {
+    }
+
+    @Test
+    public void getMoneyLevel() {
+    }
+
+    @Test
+    public void getInkLevel() {
+    }
+
+    @Test
+    public void getPaperLevel() {
+    }
+
+    @Test
+    public void authenticateCustomer() {
+    }
+
+    @Test
+    public void viewAccount() {
+    }
+
+    @Test
+    public void depositCash() {
+    }
+
+    @Test
+    public void depositCheck() {
+    }
+
+    @Test
+    public void withdraw() {
+    }
+
+    @Test
+    public void quit() {
+    }
+
+    @Test
+    public void toDataString() {
+        String[] data = testATM.toDataString().split(",");
+        assertEquals(testATM.getId(), Long.parseLong(data[0]));
+        assertEquals(1000000, Double.parseDouble(data[1]), .001);
+        assertEquals(2000, Integer.parseInt(data[2]));
+        assertEquals(2000, Integer.parseInt(data[3]));
+    }
+
+    @Test
+    public void testDataStringConstructor() {
+        ATM newTestATM = new ATM(testATM.toDataString());
+        String[] data = newTestATM.toDataString().split(",");
+        assertEquals(testATM.getId(), Long.parseLong(data[0]));
+        assertEquals(1000000, Double.parseDouble(data[1]), .001);
+        assertEquals(2000, Integer.parseInt(data[2]));
+        assertEquals(2000, Integer.parseInt(data[3]));
     }
 }
